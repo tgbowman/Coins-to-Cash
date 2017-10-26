095fe548d758b6ca01062e61ddc1ee96bc9f5199
Tyler Bowman <tylerbowman@Tylers-MacBook-Pro.local>
2017-10-26 14:37:33 -0500

    added user input

--- coinsToCash.js
+++ coinsToCash.js
@@ -1,36 +1,54 @@
-let quarters = prompt("How many quarters do you have?");
+/* let quarters = prompt("How many quarters do you have?");
 let dimes = prompt("How many dimes do you have?");
 let nickels = prompt("How many nickels do you have?");
 let pennies = prompt("How many pennies do you have?");
+ */
 
-let piggyBank = {
-    "quarters": quarters,
-    "nickels": nickels,
-    "dimes": dimes,
-    "pennies": pennies
-};
 
-let addItUp = (object) => {
+
+
+let addButton = document.getElementById("add");
+
+//Capturing user input and storing it into variables
+
+
+// storing variables in an object piggyBank
+
+
+//function that takes the properties of piggyBank and calculates the dollar amount.
+let addItUp = () => {
+    let quartersEl = parseInt(document.getElementById("quarters").value);
+    let dimesEl = parseInt(document.getElementById("dimes").value);
+    let nickelsEl = parseInt(document.getElementById("nickels").value);
+    let penniesEl = parseInt(document.getElementById("pennies").value);
+
+    let piggyBank = {
+        "quarters": quartersEl,
+        "nickels": nickelsEl,
+        "dimes": dimesEl,
+        "pennies": penniesEl
+    };
+
     let dollarAmount = 0;
 
-    for (let prop in object) {
-        if (prop === "quarters") {
-             dollarAmount += (object[prop] * .25);
+    for (let coin in piggyBank) {
+        if (coin === "quarters") {
+             dollarAmount += (piggyBank[coin] * .25);
         } else if 
-             (prop === "nickels") {
-                 dollarAmount += (object[prop] * .05);
+             (coin === "nickels") {
+                 dollarAmount += (piggyBank[coin] * .05);
             } else if 
-                (prop === "dimes") {
-                     dollarAmount += (object[prop] * .10);
+                (coin === "dimes") {
+                     dollarAmount += (piggyBank[coin] * .10);
                 } else if 
-                    (prop === "pennies") {
-                         dollarAmount += (object[prop] * .01);
+                    (coin === "pennies") {
+                         dollarAmount += (piggyBank[coin] * .01);
                     }
                 }
-                document.write('<h1>You have $' + dollarAmount + '</h1>');
+                document.write('<h1> You have $' + dollarAmount +'</h1>')
             }
  
-addItUp(piggyBank);
+addButton.addEventListener("click", addItUp);
         
     
 
